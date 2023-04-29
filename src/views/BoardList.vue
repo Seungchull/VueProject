9f4fcd0978b7371b64d757b2c1b15767e459c529
seungchull <user@example.com>
2023-04-29 16:40:48 +0900

    1st version

---
+++ src/views/BoardList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="board">
+        <h1>This is an board list page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BoardList'
+    
+}
+</script>
+
+<style scoped>
+.board {width:800px; margin: 20px auto;}</style>
